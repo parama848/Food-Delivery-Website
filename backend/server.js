eb77a8e907707db7e942056c34115e84b06428d0
parama848 <parama8148497159@gmail.com>
2026-07-10 21:48:19 +0530

    / removed

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,7 @@ app.set("trust proxy", 1);
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
-  "https://food-delivery-website-client.vercel.app/",
+  "https://food-delivery-website-client.vercel.app",
 ];
 
 app.use(
